b6ac960491cb251a92a19c6aa9cc0c1b15632792
Josh Yang <spadeg39475@gmail.com>
2019-11-03 19:44:27 +0800

    Updates

---
+++ precache-manifest.62b32dd75dd5fe31a8ba0f6d3893ae0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc955acc0bee3d0d06602b4d6a8e3bfa",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "210b21d8c162efbc3b76",
+    "url": "/tic-tac-toe/static/css/main.d9d5f10c.chunk.css"
+  },
+  {
+    "revision": "f29dfa8d9305949dd8c1",
+    "url": "/tic-tac-toe/static/js/2.983ce462.chunk.js"
+  },
+  {
+    "revision": "210b21d8c162efbc3b76",
+    "url": "/tic-tac-toe/static/js/main.dc32cf53.chunk.js"
+  },
+  {
+    "revision": "027d780fd6ac3f298ef0",
+    "url": "/tic-tac-toe/static/js/runtime-main.ecf72bd5.js"
+  }
+]);
